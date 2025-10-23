9691bfa1102376992304bee7cecfb7aec97a271f
vishnuwebdev2425 <akkupallivishnu2424@gmail.com>
2025-10-23 11:44:40 +0530

    component Composition

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-/*<div>
-    <div id="parent">
-        <div id="child">
-            <h1>Vishnu</h1>
-            <h2>Shiva</h2>
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const FirstComponent=()=>(
+   
+        <div>
+            <h1>Shiva</h1>
 
         </div>
-        <div id="child2">
-        <h1>par</h1>
-        </div>
-
-    </div>
-
-</div>
-*/
-const newElement=React.createElement("div",{id:"parent"},
-    ([React.createElement('div',{id:'child'},([React.createElement('h1',{},"Vishnu"),
-    React.createElement("h2",{},"Shiva")]),
-    React.createElement("div",{id:"child2"},
-        React.createElement("h1",{},"Parvati")
-    )
-)])
 )
+
+const SecondComponent=()=>{
+    return(<div>
+        < FirstComponent/>
+        <h2>Parvati</h2>
+    </div>)
+}
+
 const root=ReactDOM.createRoot(document.getElementById("root"))
-root.render(newElement)
+root.render(< SecondComponent/>)
